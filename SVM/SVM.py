e81d4c3ee7394fd4959c021e57ba6ce582ec282f
LS <577031670@qq.com>
2022-12-19 15:47:14 +0800

    Initial commit

---
+++ SVM/SVM.py
@@ -0,0 +1,221 @@
+from tensorflow.keras import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from six.moves import cPickle as pickle
+import numpy as np
+import os
+
+from imageio import imread
+import platform
+
+def load_pickle(f):
+    version = platform.python_version_tuple()
+    if version[0] == '2':
+        return  pickle.load(f)
+    elif version[0] == '3':
+        return  pickle.load(f, encoding='latin1')
+    raise ValueError("invalid python version: {}".format(version))
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
+        Y = np.array(Y)
+        return X, Y
+
+def load_CIFAR10(ROOT):
+    """ load all of cifar """
+    xs = []
+    ys = []
+    for b in range(1,6):
+        f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
+
+cifar10_dir = '../data/'
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+# Split the data into train, val, and test sets. In addition we will
+# create a small development set as a subset of the training data;
+# we can use this for development so our code runs faster.
+num_training = 49000
+num_validation = 1000
+num_test = 10000
+num_dev = 500
+
+# Our validation set will be num_validation points from the original
+# training set.
+mask = range(num_training, num_training + num_validation)
+X_val = X_train[mask]
+y_val = y_train[mask]
+
+# Our training set will be the first num_train points from the original
+# training set.
+mask = range(num_training)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+# We will also make a development set, which is a small subset of
+# the training set.
+mask = np.random.choice(num_training, num_dev, replace=False)
+X_dev = X_train[mask]
+y_dev = y_train[mask]
+
+# We use the first num_test points of the original test set as our
+# test set.
+mask = range(num_test)
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape)
+print('Validation data shape: ', X_val.shape)
+print('Validation labels shape: ', y_val.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_val = np.reshape(X_val, (X_val.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+X_dev = np.reshape(X_dev, (X_dev.shape[0], -1))
+#As a sanity check, print out the shapes of the data
+print('Training data shape: ', X_train.shape)
+print('Validation data shape: ', X_val.shape)
+print('Test data shape: ', X_test.shape)
+print('dev data shape: ', X_dev.shape)
+
+mean_image = np.mean(X_train, axis=0)
+X_train -= mean_image
+X_val -= mean_image
+X_test -= mean_image
+X_dev -= mean_image
+X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
+X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
+X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
+X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
+
+
+class LinearSVM:
+    def __init__(self):
+        self.W = None
+
+    def loss(self, X, y, reg):
+        """
+        Structured SVM loss function, vectorized implementation.
+
+        Inputs and outputs are the same as svm_loss_naive.
+        """
+        loss = 0.0
+        dW = np.zeros(self.W.shape)  # initialize the gradient as zero
+
+        num_train = X.shape[0]
+        scores = X.dot(self.W)
+        correct_class_score = scores[range(num_train), list(y)].reshape(-1, 1)  # (N,1)
+        margin = np.maximum(0, scores - correct_class_score + 1)
+        margin[range(num_train), list(y)] = 0
+        loss = np.sum(margin) / num_train + 0.5 * reg * np.sum(self.W * self.W)
+
+        num_classes = self.W.shape[1]
+        inter_mat = np.zeros((num_train, num_classes))
+        inter_mat[margin > 0] = 1
+        inter_mat[range(num_train), list(y)] = 0
+        inter_mat[range(num_train), list(y)] = -np.sum(inter_mat, axis=1)
+
+        dW = (X.T).dot(inter_mat)
+        dW = dW / num_train + reg * self.W
+
+        return loss, dW
+
+    def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100,
+              batch_size=200, verbose=False):
+        """
+        Train this linear classifier using stochastic gradient descent.
+
+        Inputs:
+        - X: A numpy array of shape (N, D) containing training data; there are N
+          training samples each of dimension D.
+        - y: A numpy array of shape (N,) containing training labels; y[i] = c
+          means that X[i] has label 0 <= c < C for C classes.
+        - learning_rate: (float) learning rate for optimization.
+        - reg: (float) regularization strength.
+        - num_iters: (integer) number of steps to take when optimizing
+        - batch_size: (integer) number of training examples to use at each step.
+        - verbose: (boolean) If true, print progress during optimization.
+
+        Outputs:
+        A list containing the value of the loss function at each training iteration.
+        """
+        num_train, dim = X.shape
+        num_classes = np.max(y) + 1  # assume y takes values 0...K-1 where K is number of classes
+        if self.W is None:
+            # lazily initialize W
+            self.W = 0.001 * np.random.randn(dim, num_classes)
+
+        # Run stochastic gradient descent to optimize W
+        loss_history = []
+        for it in range(num_iters):
+            X_batch = None
+            y_batch = None
+            idx_batch = np.random.choice(num_train, batch_size, replace=True)
+            X_batch = X[idx_batch]
+            y_batch = y[idx_batch]
+
+            # evaluate loss and gradient
+            loss, grad = self.loss(X_batch, y_batch, reg)
+            loss_history.append(loss)
+
+            self.W -= learning_rate * grad
+
+            if verbose and it % 100 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+
+        return loss_history
+
+    def predict(self, X):
+        """
+        Use the trained weights of this linear classifier to predict labels for
+        data points.
+
+        Inputs:
+        - X: A numpy array of shape (N, D) containing training data; there are N
+          training samples each of dimension D.
+
+        Returns:
+        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
+          array of length N, and each element is an integer giving the predicted
+          class.
+        """
+        y_pred = np.zeros(X.shape[0])
+        scores = X.dot(self.W)
+        y_pred = np.argmax(scores, axis=1)
+        return y_pred
+
+import time
+svm = LinearSVM()
+tic = time.time()
+loss_hist = svm.train(X_train, y_train, learning_rate=1e-7, reg=2.5e4,
+                      num_iters=1500, verbose=True)
+toc = time.time()
+print('That took %fs' % (toc - tic))
+plt.plot(loss_hist)
+plt.xlabel('Iteration number')
+plt.ylabel('Loss value')
+plt.show()
+y_test_pred = svm.predict(X_test)
+test_accuracy = np.mean(y_test == y_test_pred)
+print('linear SVM on raw pixels final test set accuracy: %f' % test_accuracy)
