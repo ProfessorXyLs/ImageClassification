dd7502f71f15a48c0b3420444c7b2dec35de18a1
LS <577031670@qq.com>
2022-12-21 20:47:18 +0800

    完善文件

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-
+# train_images = train_images[0:25000]
+# train_labels = train_labels[0:25000]
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
